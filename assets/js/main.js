dd0bbc77c63a05f9bd06e11cf47d5cd4a05e5fac
Arrithyo <tayok282@gmail.com>
2025-06-16 20:26:15 +0700

    add css & btn function

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+const text = document.getElementById('text')
+const body = document.getElementById('body')
+
+text.style.color = 'greenyellow'
+
+function up() {
+    text.textContent = 'UP'    
+}
+
+function left() {
+    text.textContent = 'LEFT'
+}
+
+function right() {
+    text.textContent = 'RIGHT'
+}
+
+function down() {
+    text.textContent = 'DOWN'
+}
+
+function b() {
+    text.textContent = 'B'
+}
+
+function a() {
+    text.textContent = 'A'
+}
