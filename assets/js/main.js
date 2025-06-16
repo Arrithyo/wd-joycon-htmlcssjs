d36d21beb38d242ad7c578c409ed6a4a86c4f303
Arrithyo <tayok282@gmail.com>
2025-06-16 20:40:48 +0700

    add button eventlistener

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,27 @@ function b() {
 function a() {
     text.textContent = 'A'
 }
+
+// keyboardEvent
+
+document.addEventListener('keydown', function (event){
+    let keyEvent = `${event.key}`
+    if(keyEvent == 'a'){
+        text.textContent = 'A'
+    }
+    if(keyEvent == 'b'){
+        text.textContent = 'B'
+    }
+    if(keyEvent == 'ArrowUp'){
+        text.textContent = 'UP'
+    }
+    if(keyEvent == 'ArrowLeft'){
+        text.textContent = 'LEFT'
+    }
+    if(keyEvent == 'ArrowRight'){
+        text.textContent = 'RIGHT'
+    }
+    if(keyEvent == 'ArrowDown'){
+        text.textContent = 'DOWN'
+    }
+})
